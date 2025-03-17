ad2323af1b3fc7a48563891e8b82d991b112d9db
HafidElmoudden <abdulhafid858@gmail.com>
2025-03-17 09:49:30 +0000

    Initial commit

---
+++ src/main/java/com/hafidelmoudden/springpatientsmanagement/SpringPatientsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hafidelmoudden.springpatientsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPatientsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPatientsManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hafidelmoudden/springpatientsmanagement/SpringPatientsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hafidelmoudden.springpatientsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPatientsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
