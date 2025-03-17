e429bfbd79fa9c2d352ac52249504d39bb6eefaa
HafidElmoudden <abdulhafid858@gmail.com>
2025-03-17 10:02:26 +0000

    Patient repository and entity implementation with confing modification

--- src/main/java/com/hafidelmoudden/springpatientsmanagement/SpringPatientsManagementApplication.java
+++ src/main/java/com/hafidelmoudden/springpatientsmanagement/SpringPatientsManagementApplication.java
@@ -1,13 +1,27 @@
 package com.hafidelmoudden.springpatientsmanagement;
 
+import com.hafidelmoudden.springpatientsmanagement.entities.Patient;
+import com.hafidelmoudden.springpatientsmanagement.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
-public class SpringPatientsManagementApplication {
+public class SpringPatientsManagementApplication implements CommandLineRunner {
+    @Autowired
+    private PatientRepository patientRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringPatientsManagementApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        patientRepository.save(new Patient(null, "Hafid", new Date(), false, 15));
+        patientRepository.save(new Patient(null, "Yassine", new Date(), false, 10));
+        patientRepository.save(new Patient(null, "Omar", new Date(), true, 20));
+    }
 }
