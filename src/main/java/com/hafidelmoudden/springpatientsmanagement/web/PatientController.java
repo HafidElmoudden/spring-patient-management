2acf8a87549ae03618d7db965bfbc94007a7fdc4
HafidElmoudden <abdulhafid858@gmail.com>
2025-03-24 11:04:14 +0000

    Pagination implementation

--- src/main/java/com/hafidelmoudden/springpatientsmanagement/web/PatientController.java
+++ src/main/java/com/hafidelmoudden/springpatientsmanagement/web/PatientController.java
@@ -3,9 +3,12 @@ package com.hafidelmoudden.springpatientsmanagement.web;
 import com.hafidelmoudden.springpatientsmanagement.entities.Patient;
 import com.hafidelmoudden.springpatientsmanagement.repositories.PatientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +18,12 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patientsList", patients);
+    public String index(Model model, @RequestParam(name = "page", defaultValue = "0") int p,
+                        @RequestParam(name = "size", defaultValue = "4") int s) {
+        Page<Patient> patientsPage = patientRepository.findAll(PageRequest.of(p,s));
+        model.addAttribute("patientsList", patientsPage.getContent());
+        model.addAttribute("pages", new int[patientsPage.getTotalPages()]);
+        model.addAttribute("currentPage", p);
         return "patients";
     }
 }
